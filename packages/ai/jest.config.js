aa11cb37416b8e9013d90ecd37cf3ad66a96888b
timpel <seunodueso@gmail.com>
2026-06-22 13:58:01 +0100

    test(ai): add Jest ESM config for ai package

---
+++ packages/ai/jest.config.js
@@ -0,0 +1,29 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: {
+        moduleResolution: 'Node16',
+        module: 'Node16',
+        noEmit: false,
+      },
+    }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@forumkit/types$': '<rootDir>/../../packages/types/src/index.ts',
+  },
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  coverageThreshold: {
+    global: { lines: 80 },
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+    '!src/providers/**',
+  ],
+};
